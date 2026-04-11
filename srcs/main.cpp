4dd831b563f5ada844e8e81446399c8b7401fe2a
Fefo420 <Fefo.leo51@gmail.com>
2026-04-11 10:26:06 +0200

    Closing Fd's working. Built the socket server for connecting and it should also work. We will have to check together to make sure  there is any other chases i've missed.

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jidrizi <jidrizi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fefo <fefo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 15:54:01 by jidrizi           #+#    #+#             */
-/*   Updated: 2026/04/09 16:22:38 by jidrizi          ###   ########.fr       */
+/*   Updated: 2026/04/11 10:23:08 by fefo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,52 @@ int	printError(std::string	error_msg)
 	return (1);
 }
 
+void Server::closeFds()
+{
+	//looping through all the clients and closing all of them
+	for (size_t i = 0; i < clients.size(); i++)
+	{
+		std::cout << RED << "Client <" << clients[i].getFd() << "> Disconnected" << WHI << std::endl;
+		close(clients[i].getFd());
+	}
+	//closing the server socket
+	if (servSocketfd != -1)
+	{
+		close(servSocketfd);
+		std::cout << RED << "Server <" << servSocketfd << "> Disconnected" << WHI << std::endl;
+	}
+}
+
+void Server::servSocket()
+{
+	struct sockaddr_in add;
+	struct pollfd NewPoll;
+	add.sin_family = AF_INET; //set the address to the ipv4 family
+	add.sin_port = htons(this->port); //conver port into network byte order (the big endian thing)
+	add.sin_addr.s_addr = INADDR_ANY; //for any machine addr
+
+	std::cout << "sin_port = " << add.sin_port << std::endl;
+
+	servSocketfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (servSocketfd == -1)
+		throw(std::runtime_error("Failed to create the socket"));
+		
+	int en = 1;
+	if (setsockopt(servSocketfd, SOL_SOCKET, SO_REUSEADDR, &en, sizeof(en)) == -1)
+		throw(std::runtime_error("Failed to set SO_REUSEADDR on socket"));
+	if (fcntl(servSocketfd, F_SETFD, O_NONBLOCK) == -1)
+		throw(std::runtime_error("Failed to set O_NONBLOCK on socket"));
+	if (bind(servSocketfd, (struct sockaddr *)&add, sizeof(add)) == -1)
+		throw(std::runtime_error("Failed to bind socket"));
+	if (listen(servSocketfd, SOMAXCONN) == -1)
+		throw(std::runtime_error("Failed listen()"));
+
+	NewPoll.fd = servSocketfd;
+	NewPoll.events = POLLIN;
+	NewPoll.revents = 0;
+	fds.push_back(NewPoll);
+}
+
 void Server::servInit()
 {
 	// fixed port for now, will fix it later with dynamic port
@@ -34,7 +80,7 @@ void Server::servInit()
 
 	std::cout << "Port: " << port << std::endl;
 	//start the socket somehow
-	// servSocket();
+	servSocket();
 	std::cout << GRE << "Server <" << servSocketfd << "> Connected" << WHI << std::endl;
 	std::cout <<"Waiting to connect to Server..." << std::endl;
 }
@@ -57,8 +103,9 @@ int main(int argc, char** argv)
 		serv.servInit();
 	}
 	catch(const std::exception& e){
-		std::cerr << e.what() << std::endl;
 		//close fd's if error
+		serv.closeFds();
+		std::cerr << e.what() << std::endl;
 	}
 	std::cout << "Server closed!"<< std::endl;
 	return (0);
